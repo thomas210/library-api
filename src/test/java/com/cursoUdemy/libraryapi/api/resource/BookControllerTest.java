ee2ea7e040f993db739427e73c622c66cec4687a
thomas210 <thomastabosa10@gmail.com>
2021-12-15 20:32:55 -0300

    :white_check_mark: create bookControllerTest

---
+++ src/test/java/com/cursoUdemy/libraryapi/api/resource/BookControllerTest.java
@@ -0,0 +1,34 @@
+package com.cursoUdemy.libraryapi.api.resource;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Must create a book")
+    public void createBookTest(){
+
+    }
+
+    @Test
+    @DisplayName("Must throws an validation error when there isn't enough data")
+    public void createInvalidBookTest(){
+
+    }
+
+}
